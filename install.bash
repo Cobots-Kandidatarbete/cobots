d1f97acd76a5fcedddcc466a81f835fe35b01328
gasparyanartur <gasparyanartur99@gmail.com>
2022-03-10 16:45:45 +0100

    Fix cam configuration loading

--- install.bash
+++ install.bash
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 PATH_TO_SRC="./dep_ws/src/" 
+PATH_TO_WS_SRC="./ws/src/lab/src/"
 git clone https://github.com/sequenceplanner/gui_tools.git $PATH_TO_SRC/gui_tools
 git clone https://github.com/sequenceplanner/mp_msgs.git $PATH_TO_SRC/mp_msgs
 git clone https://github.com/sequenceplanner/opcua_ros2_bridge.git $PATH_TO_SRC/opcua_ros2_bridge
@@ -10,6 +11,10 @@ git clone https://github.com/sequenceplanner/ur_tools.git $PATH_TO_SRC/ur_tools
 git clone https://github.com/sequenceplanner/viz_tools.git $PATH_TO_SRC/viz_tools
 git clone https://github.com/sequenceplanner/tf_tools_msgs.git $PATH_TO_SRC/tf_tools_msgs
 git clone https://github.com/sequenceplanner/ur_tools_msgs.git $PATH_TO_SRC/ur_tools_msgs
+
+git clone https://github.com/Cobots-Kandidatarbete/aruco_handler.git $PATH_TO_WS_SRC/aruco_handler
+git clone https://github.com/Cobots-Kandidatarbete/camera-calibration.git $PATH_TO_WS_SRC/camera-calibration
+
 cd dep_ws/src/ur_tools
 git reset --hard 850bc0440a740f85bccb25f00d5fbe642f819a6f
 cd ..
