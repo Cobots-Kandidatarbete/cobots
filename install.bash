166003dae1d5d769361d08aef746ea630f41bb55
gasparyanartur <gasparyanartur99@gmail.com>
2022-03-17 11:38:49 +0100

    fix repo

--- install.bash
+++ install.bash
@@ -12,8 +12,8 @@ git clone https://github.com/sequenceplanner/viz_tools.git $PATH_TO_SRC/viz_tool
 git clone https://github.com/sequenceplanner/tf_tools_msgs.git $PATH_TO_SRC/tf_tools_msgs
 git clone https://github.com/sequenceplanner/ur_tools_msgs.git $PATH_TO_SRC/ur_tools_msgs
 
-git clone https://github.com/Cobots-Kandidatarbete/aruco_handler.git $PATH_TO_WS_SRC/aruco_handler
-git clone https://github.com/Cobots-Kandidatarbete/camera-calibration.git $PATH_TO_WS_SRC/camera-calibration
+git clone https://github.com/Cobots-Kandidatarbete/aruco_handler.git $PATH_TO_SRC/aruco_handler
+git clone https://github.com/Cobots-Kandidatarbete/camera-calibration.git $PATH_TO_SRC/camera-calibration
 
 cd dep_ws/src/ur_tools
 git reset --hard 850bc0440a740f85bccb25f00d5fbe642f819a6f
